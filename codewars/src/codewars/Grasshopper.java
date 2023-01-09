4d67378c4f1b3219d82ea9ca61b8bce752189a18
MiloszSipka <milosz.sipka@gmail.com>
2023-01-09 10:21:34 +0000

    Grasshopper fix

--- codewars/src/codewars/Grasshopper.java
+++ codewars/src/codewars/Grasshopper.java
@@ -3,25 +3,26 @@ package codewars;
 public class Grasshopper {
 
 	public static void main(String[] args) {
-		int s1 = 58;
-		int s2 = 59;
-		int s3 = 60;
+		int s1 = 70;
+		int s2 = 70;
+		int s3 = 70;
 		System.out.println(getGrade(s1,s2,s3));
 		
 	}
 	 public static char getGrade(int s1, int s2, int s3) {
-		 int avg = (s1+s2+s3)/3;
+		 double avg = (s1+s2+s3);
+		 avg = avg/3.0;
 		 
 		 if(avg >= 90) {
 			 return 'A';
 		 }
-		 else if(avg >= 80 || avg < 90){
+		 else if(avg < 90 && avg >= 80){
 				 return 'B';
-			 }
-		 else if(avg >= 70 || avg < 80) {	
+		 }
+		 else if(avg < 80 && avg >= 70){	
 			 return 'C';
 		 }
-		 else if(avg >= 60  || avg < 70) {
+		 else if(avg < 70 && avg >= 60) {
 			 return 'D';
 		 }
 		 else
@@ -29,4 +30,5 @@ public class Grasshopper {
 			 return 'F';
 		 }
 	    }
+	 
 }
