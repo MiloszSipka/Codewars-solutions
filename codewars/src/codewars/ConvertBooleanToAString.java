984e1a89348c0665e764bfbe36077a91eb28ab15
MiloszSipka <milosz.sipka@gmail.com>
2023-01-10 15:52:50 +0000

    ConvertBooleanToString solution

---
+++ codewars/src/codewars/ConvertBooleanToAString.java
@@ -0,0 +1,17 @@
+package codewars;
+
+public class ConvertBooleanToAString {
+
+	public static void main(String[] args) {
+
+	}
+	public class BooleanToString {
+		  public static String convert(boolean b){
+		    if(b == true) {
+		      return "true";
+		    } else {
+		      return "false";
+		    }
+		  }
+	}
+}
