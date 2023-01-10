5d4263ec59d9e983f2fe77172c3109d366edb39c
MiloszSipka <milosz.sipka@gmail.com>
2023-01-10 16:03:03 +0000

    Solution

--- codewars/src/codewars/ConvertAStringToANumber.java
+++ codewars/src/codewars/ConvertAStringToANumber.java
@@ -2,4 +2,9 @@ package codewars;
 
 public class ConvertAStringToANumber {
 
-}
+
+	  public static int stringToNumber(String str) {
+		  int number = Integer.valueOf(str);
+		  return number;
+	}
+	  }
