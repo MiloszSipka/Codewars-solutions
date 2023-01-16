6211a1e5db462affcdc45adfea492fa11dea77c0
MiloszSipka <milosz.sipka@gmail.com>
2023-01-16 17:37:14 +0000

    SumMixedArray solution fixed

--- codewars/src/codewars/SumMixedArray.java
+++ codewars/src/codewars/SumMixedArray.java
@@ -1,10 +1,27 @@
 package codewars;
 
+import java.util.List;
+
 public class SumMixedArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
+	public int sum(List<?> mixed) {	
+		int sum = 0;
+	
+		for(int i = 0; i < mixed.size(); i++) {
+			Object temp = mixed.get(i);
+			if(temp instanceof Integer) {
+				int tempInt = (int) mixed.get(i);
+				sum += tempInt;
+			}
+			else {
+				String tempString = (String) mixed.get(i);
+				int tempStringToInt = Integer.parseInt(tempString);
+				sum += tempStringToInt;
+			}
+		}
+		return sum;
+}
 }
