b0e6ba4074f5837e0414560b109cae2ce6c0a42d
MiloszSipka <milosz.sipka@gmail.com>
2023-01-20 20:54:30 +0000

    ReturnString solution

---
+++ codewars/src/codewars/ReturningString.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class ReturningString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	  public static String greet(String name)
+	  {
+	    String result =  "Hello, " + name + " how are you doing today?";
+	    return result;
+	  }
+}
