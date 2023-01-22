ccede9b362e9ef1343bc92f8196afb72faa04599
MiloszSipka <milosz.sipka@gmail.com>
2023-01-22 18:28:46 +0000

    SumOfOddNumber solution

---
+++ codewars/src/codewars/SumOfoddnumbers.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class SumOfoddnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class RowSumOddNumbers {
+	    public static int rowSumOddNumbers(int n) {
+	     int result = (int) Math.pow(n, 3);
+	     return result;
+	    }
+}
