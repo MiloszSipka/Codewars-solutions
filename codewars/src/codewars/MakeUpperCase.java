9a4405f97bc45965fd6e840ea706ed39fdfea64c
MiloszSipka <milosz.sipka@gmail.com>
2023-01-27 19:52:41 +0000

    touppercase solution

---
+++ codewars/src/codewars/MakeUpperCase.java
@@ -0,0 +1,12 @@
+package codewars;
+
+public class MakeUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	  public static String MakeUpperCase(String str){
+		return str.toUpperCase();
+	  } 
+}
